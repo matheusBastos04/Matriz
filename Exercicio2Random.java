f8b1bc30986b62e3e588487cb78de2a3d364e98c
matheusBastos04 <56124197+matheusBastos04@users.noreply.github.com>
2019-11-21 08:06:22 -0300

    Add files via upload

---
+++ Exercicio2Random.java
@@ -0,0 +1,24 @@
+import java .util.Random;  	
+public class Exercicio2Random {
+	 public static void main ( String [] args) {
+	int v [][]= new int [3] [3];
+ 
+	int i, j ;
+	Random x = new Random ();
+	for ( i=0; i<=2;++i) {
+		for(j=0 ;j<=2;j++ ) {
+			v[i][j]=x.nextInt () %10;
+	
+		}
+}
+for( i=0; i<=2 ;++i){
+	for (j=0;j<=2; j++) {
+		System.out.print("[ " + v[i][j] +  " ]"  );
+if ( j==2 ) {
+System.out.print("\n");
+}
+}
+}
+}
+}
+ 
